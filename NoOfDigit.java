a24f5cc1b7c5709ba35dce64e34a62bec54aafe9
Itsnidasaifi <96730205+Itsnidasaifi@users.noreply.github.com>
2023-05-21 18:45:14 +0530

    Add files via upload

---
+++ NoOfDigit.java
@@ -0,0 +1,12 @@
+public class NoOfDigit {
+    public static void main(String[] args) {
+        int n=12345;
+        int b=12;
+        int ans=(int)(Math.log(n)/Math.log(b))+1;
+        System.out.println(ans);
+        //this will represent the number of bits in b
+        
+
+    }
+    
+}
